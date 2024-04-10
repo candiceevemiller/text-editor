a90d796870d78b88a05fa086d1cb486ff38b0369
candiceevemiller <candice.eve.miller@gmail.com>
2024-04-10 15:28:00 -0400

    add flags, set timeout, raw mode complete

--- cedit.c
+++ cedit.c
@@ -26,14 +26,17 @@ int main(void)
 {
     enableRawMode();
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c!='q') 
-    {
+    while (1) {
+        char c = '\0';
+        read(STDIN_FILENO, &c, 1);
+        
         if (iscntrl(c)) {
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         } else {
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);
         }
+
+        if (c == 'q') break;
     }
 
     return 0;
@@ -45,16 +48,61 @@ int main(void)
 
 void enableRawMode()
 {
+    // Get original terminal config
     tcgetattr(STDIN_FILENO, &orig_termios);
+
+    // Automatically run disableRawMode at program exit
     atexit(disableRawMode);
     
+    // Copy orig config into raw
     struct termios raw = orig_termios;
-    raw.c_lflag &= ~(ECHO | ICANON);
+
+    /* Set the terminal config flags to what we want using bitwise arithmetic
+     * & (and) and ~ (not) force these flags to be false
+     * All code comes from termios.h
+     *
+     * 
+     * INPUT FLAGS - c_iflags:
+     * -------------------------------
+     * IXON - handles ctrl-s ctrl-q (suspend/resume)
+     * ICRNL - CR carriage return NL new line. Replaces \r with \n on ctrl-m
+     * Likely useless on modern terminals but part of turning on raw mode:
+     * BRKINT - sends interrupt (ctrl-c SIGINT) on a break condition
+     * INPCK - checks parity
+     * ISTRIP - strips 8th bit of each input byte
+     * 
+     * OUTPUT FLAGS - c_oflags:
+     * -------------------------------
+     * OPOST - output post processing converts \n to \r\n related to ICRNL
+     *         practically this means we're using \r\n manually in this file
+     * 
+     * CONTROL FLAGS - c_cflags:
+     * -------------------------------
+     * CS8 - Not a flag, a mask with multiple bits - sets char size to 8 bits
+     *
+     * LOCAL FLAGS - c_lflags:
+     * -------------------------------
+     * ECHO - echos what you write to the terminal
+     * ICANON - reads by line instead of by byte
+     * ISIG - controls ctrl-c ctrl-z (interrupt / and suspend to background)
+     * IEXTEN - handles ctrl-v
+     * 
+     * We want byte-wise reading, no echo, override escape chars
+     */
+    raw.c_iflag &= ~(ICRNL | IXON | BRKINT | INPCK | ISTRIP);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+
+    raw.c_cc[VMIN] = 0; // Sets minimum bytes to 0 - reads input after there's any input to be read
+    raw.c_cc[VTIME] = 1; // Times out input read after 1/10th second
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 void disableRawMode()
 {
+    // Set terminal back to orignial config
+    // Flush any trailing characters (TCSAFLUSH)
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
